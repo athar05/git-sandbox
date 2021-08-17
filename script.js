6a7127a69e203d73495ce0459527111604f69674
athar05 <atharshakeel96@gmail.com>
2021-08-18 00:42:03 +0530

    wiring completed

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 const mainPassword = document.getElementById("password-main");
 const repeatedPassword = document.getElementById("password-main");
 const messageElement = document.getElementById("message");
+
+console.log(mainPassword);
+console.log(repeatedPassword);
+console.log(messageElement);
