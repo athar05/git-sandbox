03d1328085ca02a0d37e5092f8b08ee293aba6cf
athar05 <atharshakeel96@gmail.com>
2021-08-18 00:38:30 +0530

    html css skeleton

---
+++ script.js
@@ -0,0 +1,4 @@
+// setting customElements
+const mainPassword = document.getElementById("password-main");
+const repeatedPassword = document.getElementById("password-main");
+const messageElement = document.getElementById("message");
